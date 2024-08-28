3da8a9387c4bad6b0a20135131bb312286f21c68
fork-coding <danju@starlight-sangdan.com>
2024-08-28 12:36:27 +0900

    fix: fix typo of constant categoryLists

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 type UICategories = "공통" | "인풋" | "인터렉션";
 
-type CatetoryLists = {
+type CategoryLists = {
   id: UICategories;
   label: string;
 };
-export const UI_CATEGORIES: CatetoryLists[] = [
+export const UI_CATEGORIES: CategoryLists[] = [
   { id: "공통", label: "일반(공통) common" },
   { id: "인풋", label: "사용자 인풋 input" },
   { id: "인터렉션", label: "상호작용 interaction" },
